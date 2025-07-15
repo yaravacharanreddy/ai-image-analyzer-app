bdc8a86bba831996f0a72477c20d1419d73baa66
yaravacharanreddy <yarvacharan2004@gmail.com>
2025-07-15 19:08:21 +0530

    Local changes before force push

--- backend/app.py
+++ backend/app.py
@@ -41,5 +41,5 @@ def process_image():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
     
